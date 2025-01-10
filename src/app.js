8c1888f60c27959fadf79e21557062c8cf06fb23
adrianallanga <rojas-escobar@hotmail.com>
2025-01-10 20:06:15 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronombres = ["mi", "tu"];
+  let adjetivos = ["bajo", "rapido", "amable", "lindo", "feliz", "fuerte"];
+  let sustantivos = ["libro", "arbol", "gorra", "guitarra", "pluma"];
+  for (let i = 0; i < pronombres.length; i++) {
+    for (let j = 0; j < adjetivos.length; j++) {
+      for (let k = 0; k < sustantivos.length; k++) {
+        console.log(pronombres[i] + adjetivos[j] + sustantivos[k]);
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
